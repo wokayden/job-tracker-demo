fd842bc61cecfb2c02bb5480d0eff006199f8ec4
wokayden <79875981+wokayden@users.noreply.github.com>
2026-02-03 14:50:29 -0500

    load correct data for different envs, rename window -> mainWindow

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,17 +2,19 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
 
-
-
 //when app is ready, create a new browserwindow
 app.on('ready', () => {
-    const window = new BrowserWindow({ width: 500, height: 500, webPreferences: {
+    const mainWindow = new BrowserWindow({ width: 500, height: 500, webPreferences: {
         preload: path.join(__dirname, '../preload/index.js'),
         nodeIntegration: false,
         contextIsolation: true
     } });
 
-
+    if (process.env['ELECTRON_RENDERER_URL']) {
+        mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL']!)
+    } else {
+        mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'))
+    }
 });
 
 app.on('window-all-closed', () =>{
