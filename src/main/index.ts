b2d455338f81cb6556760fbafa5b168caacf37b6
wokayden <79875981+wokayden@users.noreply.github.com>
2026-02-03 14:04:17 -0500

    holy crap it opens a window!

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,1 +1,21 @@
 //main process entry point
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+
+
+
+//when app is ready, create a new browserwindow
+app.on('ready', () => {
+    const window = new BrowserWindow({ width: 500, height: 500, webPreferences: {
+        preload: path.join(__dirname, '../preload/index.js'),
+        nodeIntegration: false,
+        contextIsolation: true
+    } });
+
+
+});
+
+app.on('window-all-closed', () =>{
+    //don't go to tray
+    app.quit();
+});
