ea46a89fecf2f5bd793689ad0df53fa567ab1394
wokayden <79875981+wokayden@users.noreply.github.com>
2026-02-03 15:31:39 -0500

    very basic - can see content of app now

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,1 +1,3 @@
 //root react component
+import React from 'react';
+export default () => { return (<div style={{height: '100px', width: '100px', backgroundColor:'orange'}}></div>) }
--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,1 +1,8 @@
 //react dom mount point
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import React from "react";
+
+const root = document.getElementById("root");
+if (!root) throw new Error('Couldnt find root element');
+if (root) createRoot(root).render(<App />);
