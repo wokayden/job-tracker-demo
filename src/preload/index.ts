28ccd2370d9aa4509965f7200149de2690bceb11
wokayden <79875981+wokayden@users.noreply.github.com>
2026-02-05 11:42:19 -0500

    update has uid

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -7,7 +7,7 @@ contextBridge.exposeInMainWorld('api', {
     getApplications: () => ipcRenderer.invoke(channelKeys.getApplications),
     getApplication: (uid: string) => ipcRenderer.invoke(channelKeys.getApplication, uid),
     addApplication: (ja: JobApplication) => ipcRenderer.invoke(channelKeys.addApplication, ja),
-    updateApplication: (ja: Partial<JobApplication>) => ipcRenderer.invoke(channelKeys.updateApplication, ja),
+    updateApplication: (uid: string, ja: Partial<JobApplication>) => ipcRenderer.invoke(channelKeys.updateApplication, uid, ja),
     deleteApplication: (uid: string) => ipcRenderer.invoke(channelKeys.deleteApplication, uid),
     getSources: () => ipcRenderer.invoke(channelKeys.getSources),
     addSource: (s: string) => ipcRenderer.invoke(channelKeys.addSource, s)
