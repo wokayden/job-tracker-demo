04e1a29987e3be727ea1b50fedde8054fc839201
wokayden <79875981+wokayden@users.noreply.github.com>
2026-02-05 11:40:55 -0500

    ipc for data

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,4 +1,14 @@
 //preload bridge script
-import { contextBridge } from "electron";
+import { contextBridge, ipcRenderer } from "electron";
+import { channelKeys } from "../shared/ipc/channels";
+import { JobApplication } from "../shared/domain";
 
-contextBridge.exposeInMainWorld('api', {}) //for now nothing fancy
+contextBridge.exposeInMainWorld('api', {
+    getApplications: () => ipcRenderer.invoke(channelKeys.getApplications),
+    getApplication: (uid: string) => ipcRenderer.invoke(channelKeys.getApplication, uid),
+    addApplication: (ja: JobApplication) => ipcRenderer.invoke(channelKeys.addApplication, ja),
+    updateApplication: (ja: Partial<JobApplication>) => ipcRenderer.invoke(channelKeys.updateApplication, ja),
+    deleteApplication: (uid: string) => ipcRenderer.invoke(channelKeys.deleteApplication, uid),
+    getSources: () => ipcRenderer.invoke(channelKeys.getSources),
+    addSource: (s: string) => ipcRenderer.invoke(channelKeys.addSource, s)
+});
---
+++ src/shared/ipc/channels.ts
@@ -0,0 +1,9 @@
+export const channelKeys = {
+    getApplications: "app:get-all",
+    getApplication: "app:get-one",
+    addApplication: "app:add",
+    updateApplication: "app:update",
+    deleteApplication: "app:delete",
+    getSources: "sources:get-all",
+    addSource: "sources:add"
+} as const;
