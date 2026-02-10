54aa7073aa1b5a71157fe706c017a7e8bf549f8d
wokayden <79875981+wokayden@users.noreply.github.com>
2026-02-10 11:14:20 -0500

    same logic for archived & leads

--- src/renderer/src/pages/ArchivedList.tsx
+++ src/renderer/src/pages/ArchivedList.tsx
@@ -1,9 +1,43 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useState, useEffect } from "react";
+import { JobApplication, Status } from "../../../shared/domain";
+import { ApplicationCard } from "../components/ApplicationCard";
 
 export function ArchivedList(): ReactElement {
+    const [archivedApps, setArchivedApps] = useState<JobApplication[]>([]);
+
+    function fetchApplications() {
+        window.api.getApplications().then(r => {
+            if (r.length > 0) {
+                setArchivedApps(r.filter(a => a.status === Status.Archived));
+            };
+        });        
+    };
+
+    function onChangeApplicationStatus(uid: string, status: Status, reason?: string) {
+        window.api.updateApplication(uid, {status}).then(r => {
+            if (r.success) {
+                alert('Updated successfuly');
+                fetchApplications();
+            } else {
+                alert(`Update failed: ${r.message}`);
+            }
+        });
+    };
+
+    useEffect(() => {
+        fetchApplications();
+    }, []);
+
     return (
         <div>
-            archived list
+            <div>
+                <h1>Archived Jobs</h1>
+            </div>
+            <ul>
+                {archivedApps.map(app => {
+                    return <ApplicationCard app={app} onStatusChange={onChangeApplicationStatus} key={app.uid} />
+                })}
+            </ul>
         </div>
-    )
+    );
 }
--- src/renderer/src/pages/LeadsList.tsx
+++ src/renderer/src/pages/LeadsList.tsx
@@ -1,9 +1,43 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useState, useEffect } from "react";
+import { JobApplication, Status } from "../../../shared/domain";
+import { ApplicationCard } from "../components/ApplicationCard";
 
 export function LeadsList(): ReactElement {
+    const [appLEads, setAppLeads] = useState<JobApplication[]>([]);
+
+    function fetchApplications() {
+        window.api.getApplications().then(r => {
+            if (r.length > 0) {
+                setAppLeads(r.filter(a => a.status === Status.Lead));
+            };
+        });        
+    };
+
+    function onChangeApplicationStatus(uid: string, status: Status, reason?: string) {
+        window.api.updateApplication(uid, {status}).then(r => {
+            if (r.success) {
+                alert('Updated successfuly');
+                fetchApplications();
+            } else {
+                alert(`Update failed: ${r.message}`);
+            }
+        });
+    };
+
+    useEffect(() => {
+        fetchApplications();
+    }, []);
+
     return (
         <div>
-            leads list
+            <div>
+                <h1>Job Leads</h1>
+            </div>
+            <ul>
+                {appLEads.map(app => {
+                    return <ApplicationCard app={app} onStatusChange={onChangeApplicationStatus} key={app.uid} />
+                })}
+            </ul>
         </div>
-    )
+    );
 }
